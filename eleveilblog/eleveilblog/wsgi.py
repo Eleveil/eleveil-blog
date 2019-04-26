0ea8eeb9db9d9874e594cca0b393cc72e9d45b50
Eleveil <davy0328meng@gmail.com>
2019-04-26 14:14:27 +0800

    初始化提交

--- eleveilblog/eleveilblog/wsgi.py
+++ eleveilblog/eleveilblog/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eleveilblog.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eleveilblog.settings")
+profile = os.environ.get('ELEVEILBLOG_PROFILE','develop')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","eleveilblog.settings.%s" % profile)
 
 application = get_wsgi_application()
--- eleveilblog/manage.py
+++ eleveilblog/manage.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eleveilblog.settings")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eleveilblog.settings")
+    profile = os.environ.get('ELEVEILBLOG_PROFILE', 'develop')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.%s" % profile)
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
